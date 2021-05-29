2624f598da504697e689ace5955a95394c4af032
serghei-pv <pavlov.serghei@web.de>
2021-05-29 19:39:00 +0200

    P3.1

--- P3/Praktikumsaufgabe_P3.1/js/server.ts
+++ P3/Praktikumsaufgabe_P3.1/js/server.ts
@@ -22,8 +22,8 @@ export namespace P3_1 {                             //export ist nur da, damit T
                                                                                                     //Typ Http.ServerResponse entgegen.
         console.log("I hear voices!");                                      //Falls die Funktion ausgeführt wird, wird in der Konsole "I hear voices!" ausgegeben.
         _response.setHeader("content-type", "text/html; charset=utf-8");    //Der Header der Serverantwort wird bearbeitet. Hier wie die Serverantwort aussieht.
-        _response.setHeader("Access-Control-Allow-Origin", "*");            //Der Header der Serverantwort wird bearbeitet. Hier wer auf den Server zu greifen darf (alle)
-        _response.write(_request.url);                                      //Die Antwort des Servers "schreibt" die url der Anfrage.
+        _response.setHeader("Access-Control-Allow-Origin", "*");            //Der Header der Serverantwort wird bearbeitet. Hier wer auf den Server zugreifen darf (alle)
+        _response.write(_request.url);                                      //Die Antwort Servers wird "geschrieben".
         console.log(_request.url);
         _response.end();                                                    //Die Antwort des Servers wird beendet.
     }
